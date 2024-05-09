fed59e222e6827fce3029dc5caeb8c4a8a55e2fa
aaronhedger <aaron.hdgr@eduge.ch>
2024-05-09 12:23:56 +0200

    Initial commit

--- cards/urls.py
+++ cards/urls.py
@@ -1,35 +1,16 @@
 # cards/urls.py
 from typing import List, Any
-
 from django.urls import path
-# Removed: from django.views.generic import TemplateView
-
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.core.wsgi import get_wsgi_application
+from django.conf.urls.static import static
 from . import views
 
-urlpatterns = [
-    path(
-        "",
-        views.CardListView.as_view(),
-        name="card-list"
-    ),
-
-    path(
-        "new",
-        views.CardCreateView.as_view(),
-        name="card-create"
-    ),
-
-    path(
-        "edit/<int:pk>",
-        views.CardUpdateView.as_view(),
-        name="card-update"
-    ),
-
-    path(
-        "box/<int:box_num>",
-        views.BoxView.as_view(),
-        name="box"
-    ),
-    path('', views.flashcard_list, name='flashcard_list'),
-
-]
+urlpatterns = ([
+    path("", views.welcome_page_view, name="welcome'"),
+    path('existing-cards/', views.existing_cards_view, name='existing_cards'),
+    path('create-cards/', views.create_cards_view, name='create_cards'),
+    path("explore/", views.explore_view, name='explore'),
+    path("start-cards/", views.start_cards_view, name='start_cards'),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
