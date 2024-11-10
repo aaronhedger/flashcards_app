6d257cfb875155d201b756495b74211e146d8865
aaronhedger <aaron.hdgr@eduge.ch>
2024-11-10 22:28:44 +0100

    voc_allemand 1 liste

--- cards/views.py
+++ cards/views.py
@@ -19,6 +19,7 @@ from .forms import CardCheckForm
 from .forms import ClasseurForm, SignUpForm
 from .models import Card, Flashcard
 from .models import Classeur
+from .cards_data import voc_allemand1
 
 
 def register(request):
@@ -28,7 +29,7 @@ def register(request):
             user = form.save()
             messages.success(request, 'compte crée avec succès')
 
-            login(request, user) # connecte directement l'utilisateur après l'incript
+            login(request, user)  # connecte directement l'utilisateur après l'incript
             return redirect('create_cards')  # redirige à la biblio
     else:
         form = SignUpForm()
@@ -100,29 +101,6 @@ def flashcard_view(request):
     return render(request, 'cards/existing_classeur/classeur_sujet/voc_all2.html', context)
 
 
-card_data = [
-    {"index": 1, "title": "German Voc 1", "front_content": "der Vater", "back_content": "the father"},
-    {"index": 2, "title": "German Voc 2", "front_content": "die Mutter", "back_content": "the mother"},
-    {"index": 3, "title": "German Voc 3", "front_content": "der Sohn", "back_content": "the son"},
-    {"index": 4, "title": "German Voc 4", "front_content": "die Tochter", "back_content": "the daughter"},
-    {"index": 5, "title": "German Voc 5", "front_content": "die Großmutter", "back_content": "the grandmother"},
-    {"index": 6, "title": "German Voc 6", "front_content": "der Großvater", "back_content": "the grandfather"},
-    {"index": 7, "title": "German Voc 7", "front_content": "der Bruder", "back_content": "the brother"},
-    {"index": 8, "title": "German Voc 8", "front_content": "die Schwester", "back_content": "the sister"},
-    {"index": 9, "title": "German Voc 9", "front_content": "der Onkel", "back_content": "the uncle"},
-    {"index": 10, "title": "German Voc 10", "front_content": "die Tante", "back_content": "the aunt"},
-    {"index": 11, "title": "German Voc 11", "front_content": "der Neffe", "back_content": "the nephew"},
-    {"index": 12, "title": "German Voc 12", "front_content": "die Nichte", "back_content": "the niece"},
-    {"index": 13, "title": "German Voc 13", "front_content": "der Cousin", "back_content": "the cousin (male)"},
-    {"index": 14, "title": "German Voc 14", "front_content": "die Cousine", "back_content": "the cousin (female)"},
-    {"index": 15, "title": "German Voc 15", "front_content": "die Enkelin", "back_content": "the granddaughter"},
-    {"index": 16, "title": "German Voc 16", "front_content": "der Enkel", "back_content": "the grandson"},
-    {"index": 17, "title": "German Voc 17", "front_content": "die Schwiegermutter",
-     "back_content": "the mother-in-law"},
-    {"index": 18, "title": "German Voc 18", "front_content": "der Schwiegervater",
-     "back_content": "the father-in-law"}
-]
-current_card_data = []  # Une variable pour stocker les cartes en cours d'affichage
 
 
 def voc_all2_view(request):
@@ -218,11 +196,10 @@ class CardCreateView(CreateView):
     success_url = reverse_lazy("card-create")
 
     def form_valid(self, form):
-
-        user = self.request.user # permet de récupérer l'utilisateur actuel
+        user = self.request.user  # permet de récupérer l'utilisateur actuel
 
         classeur_id = self.kwargs.get('classeur_id')  # Récupérer l'ID du classeur depuis l'URL
-        classeur = get_object_or_404(Classeur, id=classeur_id, user=user) # récup. objet Classeur dans la base de donn.
+        classeur = get_object_or_404(Classeur, id=classeur_id, user=user)  # récup. objet Classeur dans la base de donn.
 
         # Associer la carte au classeur et à l'utilisateur
         form.instance.classeur = classeur
@@ -400,7 +377,7 @@ def retour(request):
 
 
 def voc_all1_view(request):
-    return render(request, "cards/existing_classeur/classeur_sujet/voc_all1.html")
+    return render(request, "cards/existing_classeur/classeur_sujet/voc_all1.html", {"card_data": voc_allemand1})
 
 
 def sans_connections_view(request):
